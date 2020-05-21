bf1975ef1902c856d18f03e55e7dca0c0447281a
hzj <825840038@qq.com>
2020-05-21 16:46:45 +0800

    Initial commit

---
+++ src/mainPack/LexicalAnalysis.java
@@ -0,0 +1,54 @@
+package mainPack;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class LexicalAnalysis {
+    String state="start";
+    File file;
+    FileInputStream fileInputStream;
+    InputStreamReader inputStreamReader;
+    StringBuffer stringBuffer;
+    char[] content;
+
+    public LexicalAnalysis(String rute) throws IOException { //构造函数读文件
+        file=new File(rute);
+        if(!file.exists()){
+            System.out.println("the file does not exist");
+        }
+
+        fileInputStream = new FileInputStream(file);
+        inputStreamReader=new InputStreamReader(fileInputStream);
+        stringBuffer=new StringBuffer();
+        while (inputStreamReader.ready()){
+            stringBuffer.append((char)inputStreamReader.read());
+        }
+        content=stringBuffer.toString().toCharArray();
+    }
+
+    public String stateTransform(){
+        int correntLo=0;
+        String currentToken=null;
+        while(true){
+            switch (state){
+                case "start":
+                    if (Character.isDigit(content[correntLo])){
+                        state="innum";
+                    }
+                    else if (Character.isAlphabetic(content[correntLo])){
+                        state="inid";
+                    }
+                    else if ()
+                    break;
+            }
+            break;
+        }
+        return currentToken;
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+        LexicalAnalysis lexicalAnalysis=new LexicalAnalysis(str);
+    }
+}
+
